1bef9a0cec70d8c11c636418bd996030b9f5b50c
Xuanyang Lin <75073738+xxlin-haa@users.noreply.github.com>
2022-10-11 11:53:05 -0400

    survey app

---
+++ survey/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Survey
+
+class survey_form(forms.ModelForm):
+	class Meta:
+		model = Survey
+		fields = [
+		   'user_name',
+		   'year',
+           'rent',
+           'move_in_date',
+           'num_of_rm', 
+           'location', 
+           'getup_time', 
+           'bed_time', 
+           'bring_people', 
+           'animal', 
+           'instrument', 
+           'clean', 
+           'cook', 
+           'share', 
+           'smoke', 
+           'alcohol'
+		]
---
+++ survey/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Survey
+from .forms import survey_form
+
+# Create your views here.
+
+def survey_info(request):
+	form = survey_form(request.POST or None)
+	if form.is_valid():
+		form.save()
+
+	context = {
+	           'form':form
+	           }
+
+	return render(request, 'survey.html', context)
+
+
+
