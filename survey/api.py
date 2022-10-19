2f68f724ba7f365c35ec9111fcae5843ee939dc0
Xuanyang Lin <75073738+xxlin-haa@users.noreply.github.com>
2022-10-19 10:28:04 -0400

    Update api.py

--- survey/api.py
+++ survey/api.py
@@ -1,11 +1,66 @@
 from ninja import Router
 
 from recommender.schema import Error
-from survey.models import Survey
+from survey.models import Survey_1, Survey_2
 from survey.schema import SurveySchema
 
 survey_router = Router()
 
+
+api = NinjaAPI()
+
+
+class Survey1Schema(Schema):
+    is_authenticated: bool
+    user_id: str
+    user_name: str
+    email: str
+    y: int
+    rent: int
+    move_in_date: int
+    num_of_rm: int
+    location: str
+    getup_time: int
+    bed_time: int
+    
+    
+
+
+class Survey2Schema(Schema):
+    social: int
+    academic: int
+    bring_people: int
+    animal: int
+    instrument: int
+    clean: int
+    cook: int
+    share: int
+    smoke: int
+    alcohol: int
+
+
+
+@api.post("/survey1")
+def create_survey1(request):
+    Survey_1.objects.create()
+    return {}
+
+@api.post("/survey2")
+def create_survey2(request):
+    Survey_2.objects.create()
+    return {'success': True}
+
+
+class Q1(Schema):
+    user_name: str
+
+@api.put("/q1/{user_id}")
+def q1(request, user_id: int, usr_name: Q1):
+    user = get_object_or_404(Survey_1, id=user_id)
+    user,user_name = Q1.user_name
+    return {'success': True}
+
+"""
 
 @survey_router.post("/create-survey")
 def create_survey(request, payload: SurveySchema):
@@ -45,3 +100,4 @@ def get_survey_or_404(user_id: int):
     except Survey.DoesNotExist:
         return 404
     return survey
+"""
